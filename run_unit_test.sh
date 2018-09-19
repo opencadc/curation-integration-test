b03bb4c7e6d2249dd68ed144e9b121d1b2e0def0
Sharon <Sharon.Goliath@nrc-cnrc.gc.ca>
2018-09-18 17:22:55 -0700

    Add unit test cases for DRAO.

--- run_unit_test.sh
+++ run_unit_test.sh
@@ -18,13 +18,15 @@ do
 done
 
 # build the containers
-for ii in caom2pipe cgps omm vlass drao26m draost draosfm
+for ii in caom2utils caom2pipe cgps omm vlass drao26m draost draosfm
 do
   docker build -f ./Dockerfile.unit.${ii} -t ${ii}_unit ./ || exit $?
 done
 
 # run the unit tests for each of the modules
-docker run --rm -w /usr/src/app/caom2tools/caom2pipe caom2pipe_unit python setup.py test
+docker run --rm -w /usr/src/app/caom2tools/caom2utils caom2utils_unit python setup.py test || exit $?
+docker run --rm -w /usr/src/app/caom2tools/caom2pipe caom2pipe_unit python setup.py test || exit $?
+
 for ii in cgps omm vlass drao26m draost draosfm
 do
   docker run --rm -w /usr/src/app/${ii}2caom2 ${ii}_unit python setup.py test || exit $?
