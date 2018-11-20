c214e77d7baf8760ea8ae294def64c9b1dcdd3db
Sharon <Sharon.Goliath@nrc-cnrc.gc.ca>
2018-11-20 07:59:21 -0800

    s2378 - add support for gem2caom2, plus make it possible to run the unit tests for only one of the collections at a time.

--- run_unit_test.sh
+++ run_unit_test.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
 ROOT_DIR="${HOME}/work/cadc/dev"
+COLLECTIONS=( gem cgps omm vlass drao26m draost draosfm draogmims askap )
+
+# provide a collection name as a parameter to 'run just one' set of
+# unit tests
+if [[ $# -eq 1 ]]
+then
+  test_set=( "${1}" )
+else
+  test_set=( ${COLLECTIONS[@]} )
+fi
+temp=( )
+for ii in "${test_set[@]}"
+do
+  temp=( ${temp[@]} "${ii}2caom2" )
+done
+copy_set=( caom2tools ${temp[@]} ) 
+build_set=( caom2utils caom2pipe ${test_set[@]} ) 
 
 # clean up, before I run out of space
 # remove stopped images
@@ -24,8 +41,7 @@ then
 fi
 
 
-# for ii in caom2tools gem2caom2 cgps2caom2 omm2caom2 vlass2caom2 drao26m2caom2 draosfm2caom2 draost2caom2 draogmims2caom2
-for ii in caom2tools gem2caom2 
+for ii in "${copy_set[@]}"
 do
   sudo rsync -ai --delete ${ROOT_DIR}/${ii}/ ./${ii}_unit \
     --exclude=".git"  \
@@ -37,21 +53,30 @@ do
     --exclude="__pycache__" || exit $?
 done
 
-# build the containers
-#for ii in caom2utils caom2pipe gem cgps omm vlass drao26m draost draosfm draogmims
-for ii in caom2utils caom2pipe gem 
+echo "Copy credentials into the gemini container - note that dockerignore says ignore cadcproxy.pem"
+cp $HOME/.ssl/cadcproxy.pem ./gem2caom2_unit/proxy.pem || exit $?
+
+echo "Build the containers"
+for ii in "${build_set[@]}"
 do
   docker build -f ./Dockerfile.unit.${ii} -t ${ii}_unit ./ || exit $?
 done
 
-# run the unit tests for each of the modules
+echo "Run the unit tests for each of the modules."
 docker run --rm -w /usr/src/app/caom2tools/caom2utils caom2utils_unit python setup.py test || exit $?
 docker run --rm -w /usr/src/app/caom2tools/caom2pipe caom2pipe_unit python setup.py test || exit $?
 
-# for ii in gem cgps omm vlass drao26m draost draosfm draogmims
-for ii in gem 
+for ii in "${test_set[@]}"
 do
-  docker run --rm -w /usr/src/app/${ii}2caom2 ${ii}_unit python setup.py test || exit $?
+  echo "docker run for ${ii}"
+  output=$(docker run --rm -w /usr/src/app/${ii}2caom2 ${ii}_unit python setup.py test 2>&1)
+  result=$?
+  if [[ ${result} -ne 0 ]]
+  then
+    echo "docker run failed for ${ii}"
+    echo "${output}"
+    exit -1
+  fi
 done
 echo -n 'Success at: '
 date
